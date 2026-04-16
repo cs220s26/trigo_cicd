3343e0a60e1fc86e1619cf7830ab5536c6413226
RileyTrigo <rileytrigo@gmail.com>
2026-04-15 21:54:24 -0400

    add repo name to userdata.sh script and web_counter.service

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install git -y
-git clone https://github.com/cs220s26/<repo_name> /<repo_name>
-cd /<repo_name>
+git clone https://github.com/cs220s26/trigo_cicd /trigo_cicd
+cd /trigo_cicd
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
 cp web_counter.service /etc/systemd/system
